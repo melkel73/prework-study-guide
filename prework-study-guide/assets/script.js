b5fb07ac1bee432fb64efbf3488171d2c348d89c
MelKelair <jazzythecat73@gmail.com>
2023-02-15 18:12:26 -0500

    javascript commit

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,8 +1,13 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-for (var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
-   }
-/*
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+      console.log(topics[x]);
+    }
+  }
+listTopics()
+
+var topic = "HTML";
+function selectTopic() {
 if (topic === 'HTML') {
  console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -14,4 +19,6 @@ if (topic === 'HTML') {
 } else {
  console.log('Please try again!');
 }
-*/
+   }
+
+selectTopic()
